abd94d2a7ef2141dd72c7b6bd5ed3fea48fae8fe
Pranav Sonawane <pranavsonawane2907@gmail.com>
2025-11-30 08:51:57 +0530

    Doubly Linked List(Template)

---
+++ LinkedList12.c
@@ -0,0 +1,24 @@
+/*
+	Description :- Doubly Linked List(Template)
+	Author :- Pranav R Sonawane
+*/
+#include<stdio.h>
+#include<stdlib.h>
+  
+struct node
+{
+	int data;
+	struct node *next;
+	struct node *prev;
+};
+
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;
+
+int main()
+{
+	PNODE Head = NULL;
+	 
+	return 0; 
+}

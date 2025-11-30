4655fd6fa2bddc3ddec96686f6ddc659323180be
Pranav Sonawane <pranavsonawane2907@gmail.com>
2025-11-30 08:39:41 +0530

    Create Structure

---
+++ Structure.c
@@ -0,0 +1,19 @@
+/*
+	Description :- Create Structure
+	Author :- Pranav R Sonawane
+*/
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+	int data;
+	struct node *next;
+};
+
+int main()
+{
+	struct node *Head = NULL; 
+	
+	return 0;
+}   

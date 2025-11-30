22c3638d32ff19a0f83e1afd96f94067409bac31
Pranav Sonawane <pranavsonawane2907@gmail.com>
2025-11-30 08:40:26 +0530

    Create Structure And Memory Allocation

---
+++ Structure1.c
@@ -0,0 +1,26 @@
+/*
+	Description :- Create Structure And Memory Allocation
+	Author :- Pranav R Sonawane
+*/
+#include<stdio.h>
+#include<stdlib.h>
+
+#pragma pack(1)
+struct node
+{
+	int data;
+	struct node *next;
+};
+
+int main()
+{
+	struct node *newn = (struct node*)malloc(sizeof(struct node));
+	
+	newn->data = 11;
+	newn->next = NULL;
+	
+	printf("%d\n",newn->data);
+	printf("Size of structure is : %d\n",sizeof(struct node));
+	
+	return 0;
+}

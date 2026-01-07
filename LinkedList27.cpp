81fac398717dc8e8dabeac9632bdecfd428b6f86
Pranav Sonawane <pranavsonawane2907@gmail.com>
2026-01-07 21:40:30 +0530

    Singly Linkedlist (Insert Last)

---
+++ LinkedList27.cpp
@@ -0,0 +1,157 @@
+/*
+	Description :- Singly Linkedlist (Insert Last)
+	Author :- Pranav R Sonawane
+*/
+#include<iostream>
+using namespace std;
+
+#pragma pack(1)
+struct node
+{
+	int data;
+	struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;
+
+class SinglyLL
+{
+	public :
+		// Characteristics
+		PNODE First;
+		int iCount;
+		
+		// Behaviours
+		SinglyLL();
+		
+		void InsertFirst(int no);
+		void InsertLast(int no );
+		void InsertAtPosition(int no,int ipos);
+		
+		void DeleteFirst();
+		void DeleteLast();
+		void DeleteAtPosition(int ipos);
+		
+		void Display();
+};
+
+/*
+Return_Value Class_Name :: Function_Name(Parameters)
+{
+	
+}
+*/
+
+SinglyLL :: SinglyLL()
+{
+	First = NULL;
+	iCount = 0;
+}
+
+void SinglyLL :: InsertFirst(int no)
+{
+	// Step1 : Allocate memory for node
+	PNODE newn = new NODE;
+	
+	// Step2 : Initialise node
+	newn->data = no;
+	newn->next = NULL;
+	
+	//Step3 : Check if LL is empty or not
+	if(First == NULL)
+	{
+		First = newn;
+		iCount++;
+	}
+	else	// If LL contains atleast one node
+	{
+		newn->next = First;
+		First = newn;
+		iCount++;
+	}
+}
+
+void SinglyLL :: InsertLast(int no)
+{
+	// Step1 : Allocate memory for node
+	PNODE newn = new NODE; 
+	
+	// Step2 : Initialise node
+	newn->data = no; 
+	newn->next = NULL;
+	
+	//Step3 : Check if LL is empty or not
+	if(First == NULL)
+	{
+		First = newn;
+		iCount++;
+	}
+	else	// If LL contains atleast one node
+	{
+		PNODE temp = First;
+		
+		while(temp->next != NULL)
+		{
+			temp = temp->next;
+		}
+		temp->next = newn;
+		iCount++;
+	}
+}
+
+void SinglyLL :: InsertAtPosition(int ino, int ipos)
+{
+	
+}
+
+void SinglyLL :: DeleteFirst()
+{
+	
+}
+
+void SinglyLL :: DeleteLast()
+{
+	
+}
+
+void SinglyLL :: DeleteAtPosition(int ipos)
+{
+	
+}
+
+void SinglyLL :: Display()
+{
+	cout<<"Elements of Linkedlist are : "<<"\n";
+	PNODE temp = First;
+	
+	while(temp != NULL)
+	{
+		cout<<"| "<<temp->data<<" |->";
+		temp = temp->next;
+	}
+	cout<<"NULL"<<"\n";
+}
+
+int main()
+{
+	SinglyLL obj1;
+	
+	obj1.InsertFirst(51);
+	obj1.InsertFirst(21);
+	obj1.InsertFirst(11);
+	
+	obj1.Display();
+	cout<<"Number of node in first LinkedList are "<<obj1.iCount<<"\n";
+	
+	
+	obj1.InsertLast(101);
+	obj1.InsertLast(111);
+	obj1.InsertLast(121);
+	
+	obj1.Display();
+	cout<<"Number of node in first LinkedList are: "<<obj1.iCount<<"\n";
+	
+	return 0;
+}

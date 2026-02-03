e9f71a365df487c50749c6e40f4969bb6c95fa8d
Pranav Sonawane <pranavsonawane2907@gmail.com>
2026-02-04 00:08:10 +0530

    Addition of Digits

---
+++ LinkedList53.c
@@ -0,0 +1,93 @@
+/*
+	Description :- Addition of Digits
+	Author :- Pranav R Sonawane
+*/
+#include<stdio.h>
+#include<stdlib.h>
+
+#pragma pack(1)
+struct node 
+{
+	int data;
+	struct node* next;
+};
+ 
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;
+
+void InsertLast(PPNODE First, int no)
+{
+	PNODE newn = (PNODE)malloc(sizeof(NODE));  // Step 1 = Allocate Memory
+	PNODE temp = *First;
+	
+	newn->data = no;
+	newn->next = NULL;
+	
+	if(*First == NULL) 	// If Linked List is empty
+	{
+		*First = newn;
+	}
+	else	// If Linked List contains atleast one node
+	{
+		while(temp->next != NULL)
+		{
+			temp = temp -> next;
+		}
+		temp -> next = newn;
+	}
+}
+
+void Display(PNODE First)
+{
+	printf("Element from the linked list are : \n");
+	
+	while(First != NULL)
+	{
+		printf("| %d | -> ",First->data);
+		First = First -> next;
+	}
+	printf("NULL \n");
+}
+
+int SumDigits(int iNo)
+{
+	int iSum = 0, iDigit = 0;
+	
+	while(iNo != 0)
+	{
+		iDigit = iNo % 10;
+		iSum = iSum + iDigit;
+		iNo = iNo / 10;
+	}
+	return iSum;
+}
+
+void DisplayDigitsSum(PNODE First)
+{
+	int iRet = 0;
+	
+	while(First != NULL)
+	{
+		iRet = SumDigits(First->data);
+		printf("Addition of digits of the number %d is %d\n",First->data, iRet);
+		First = First->next;
+	}
+}
+
+int main()
+{	
+	PNODE Head = NULL;
+	
+	InsertLast(&Head, 11);
+	InsertLast(&Head, 21);
+	InsertLast(&Head, 51);
+	InsertLast(&Head, 101);
+	InsertLast(&Head, 111);
+	InsertLast(&Head, 121);
+	
+	DisplayDigitsSum(Head);
+	
+	return 0;
+}
+ 

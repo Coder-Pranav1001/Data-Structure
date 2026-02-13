9a318ea2cead91beb7590841a5a8be772f67e6ce
Pranav Sonawane <pranavsonawane2907@gmail.com>
2026-02-13 14:57:30 +0530

    Display Reverse Function of a Singly linked list in recursion

---
+++ Recursion69.c
@@ -0,0 +1,90 @@
+/*
+	Description :- DisplayReverse Function of a Singly linked list in recursion
+	Author :- Pranav R Sonawane
+*/
+#include<stdio.h>
+#include<stdlib.h>
+
+#pragma pack(1)
+struct node 
+{
+	int data;
+	struct node* next;
+};
+ 
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;
+
+void InsertLast(PPNODE First, int no)
+{
+	PNODE newn = (PNODE)malloc(sizeof(NODE));  // Step 1 = Allocate Memory
+	PNODE temp = *First;
+	
+	newn->data = no;
+	newn->next = NULL;
+	
+	if(*First == NULL) 	// If Linked List is empty
+	{
+		*First = newn;
+	}
+	else	// If Linked List contains atleast one node
+	{
+		while(temp->next != NULL)
+		{
+			temp = temp -> next;
+		}
+		temp -> next = newn;
+	}
+}
+
+void DisplayR(PNODE First)
+{	
+	if(First != NULL)
+	{
+		printf("| %d | -> ",First->data);
+		First = First -> next;
+		DisplayR(First);
+	}
+}
+
+int CountR(PNODE First)
+{
+	static int iCnt = 0;
+	
+	if(First != NULL)
+	{
+		iCnt++;
+		First = First -> next;
+		CountR(First);
+	}
+	return iCnt;	
+}
+
+void DisplayReverse(PNODE First)
+{	
+	if(First != NULL)
+	{
+		DisplayR1(First -> next); // Head Recursion // this is the only one way by using which you can display the contains of singlay linked list in revers pannel 
+		printf("| %d | -> ",First->data);
+	}
+}
+
+int main()
+{	
+	PNODE Head = NULL;
+	int iRet = 0;
+	
+	InsertLast(&Head, 11);
+	InsertLast(&Head, 21);
+	InsertLast(&Head, 51);
+	InsertLast(&Head, 101);
+	
+	DisplayReverse(Head);
+	
+	iRet = CountR(Head);
+	printf("\nNumber of nodes are : %d\n",iRet);
+	
+	return 0;
+}
+ 

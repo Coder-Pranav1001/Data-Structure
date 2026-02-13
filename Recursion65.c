9b288300ecd6f84b66b92c04a186fb44c38289dc
Pranav Sonawane <pranavsonawane2907@gmail.com>
2026-02-13 14:56:22 +0530

    LinkList Insert Last

---
+++ Recursion65.c
@@ -0,0 +1,82 @@
+/*
+	Description :- LinkList Insert Last
+	Author :- Pranav R Sonawane
+*/
+#include<stdio.h>
+#include<stdlib.h>
+
+#pragma pack(1)
+struct node 
+{
+	int data;
+	struct node* next;
+};
+ 
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;
+
+void InsertLast(PPNODE First, int no)
+{
+	PNODE newn = (PNODE)malloc(sizeof(NODE));  // Step 1 = Allocate Memory
+	PNODE temp = *First;
+	
+	newn->data = no;
+	newn->next = NULL;
+	
+	if(*First == NULL) 	// If Linked List is empty
+	{
+		*First = newn;
+	}
+	else	// If Linked List contains atleast one node
+	{
+		while(temp->next != NULL)
+		{
+			temp = temp -> next;
+		}
+		temp -> next = newn;
+	}
+}
+
+void Display(PNODE First)
+{
+	printf("Element from the linked list are : \n");
+	
+	while(First != NULL)
+	{
+		printf("| %d | -> ",First->data);
+		First = First -> next;
+	}
+	printf("NULL \n");
+}
+
+int Count(PNODE First)
+{
+	int iCnt = 0;
+	
+	while(First != NULL)
+	{
+		iCnt++;
+		First = First -> next;
+	}
+	return iCnt;	
+}
+
+int main()
+{	
+	PNODE Head = NULL;
+	int iRet = 0;
+	
+	InsertLast(&Head, 11);
+	InsertLast(&Head, 21);
+	InsertLast(&Head, 51);
+	InsertLast(&Head, 101);
+	
+	Display(Head);
+	
+	iRet = Count(Head);
+	printf("Number of nodes are : %d\n",iRet);
+	
+	return 0;
+}
+ 

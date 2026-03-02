aab6764826d4418819e25e9198f24a82afccebae
Pranav Sonawane <pranavsonawane2907@gmail.com>
2026-03-02 23:58:47 +0530

    Sorting (Bubble Sort with Handling Increasing Or Decreasing Order)

---
+++ Sorting87.java
@@ -0,0 +1,136 @@
+/*
+	Description :- Sorting (Bubble Sort with Handling Increasing Or Decreasing Order)
+	Author :- Pranav R Sonawane
+*/
+import java.lang.*;
+import java.util.*;
+
+interface MarvellousSort
+{
+	public void Accept();
+	public void Display();
+	public void BubbleSort();
+	public void InsertionSort();
+	public void SelectionSort();
+}
+
+class Array implements MarvellousSort
+{
+	public int Arr[];
+	
+	public Array(int iSize)
+	{
+		Arr = new int[iSize];
+	}
+	
+	public void Accept()
+	{
+		Scanner sobj = new Scanner(System.in);
+		System.out.println("Enter the element : ");
+		for(int i = 0; i < Arr.length; i++)
+		{
+			Arr[i] = sobj.nextInt();
+		}
+	}
+	
+	public void Display()
+	{
+		System.out.println("Elements of the array are : ");
+		for(int i = 0; i < Arr.length; i++)
+		{
+			System.out.print(Arr[i]+" ");
+		}
+		System.out.println();
+	}
+	
+	public void BubbleSort(){}
+	
+	public void BubbleSort(char type)
+	{
+		int i = 0, j = 0, temp = 0;
+		boolean Swap = false;
+		
+		if(type == 'I' || type == 'i')
+		{
+			for(i = 0; i < Arr.length; i++)
+			{
+				for(j = 0; j < Arr.length-i-1; j++)
+				{
+					if(Arr[j] > Arr[j+1])
+					{
+						temp = Arr[j];
+						Arr[j] = Arr[j+1];
+						Arr[j+1] = temp;
+						Swap = true;
+					}
+				}
+				System.out.println("Pass :"+(i + 1));
+				Display();
+				if(Swap == false)
+				{
+					break;
+				}
+			}
+		}
+		else if(type == 'd' || type == 'D')
+		{
+			for(i = 0; i < Arr.length; i++)
+			{
+				for(j = 0; j < Arr.length-i-1; j++)
+				{
+					if(Arr[j] < Arr[j+1])
+					{
+						temp = Arr[j];
+						Arr[j] = Arr[j+1];
+						Arr[j+1] = temp;
+						Swap = true;
+					}
+				}
+				System.out.println("Pass :"+(i + 1));
+				Display();
+				if(Swap == false)
+				{
+					break;
+				}
+			}
+		}
+	}
+	 
+	public void InsertionSort()
+	{
+		
+	}
+	
+	public void SelectionSort()
+	{
+		
+	}
+}
+
+class Sorting87
+{
+	public static void main(String a[])
+	{
+		Scanner sobj = new Scanner(System.in);
+		
+		System.out.println("Enter the size of array : ");
+		int size = sobj.nextInt();
+		
+		Array aobj = new Array(size);
+		
+		aobj.Accept();
+		aobj.Display();
+		
+		aobj.BubbleSort('I'); // When you pass I as a parameter it will solve Increasing Order 
+		System.out.println("----------------------------------");
+		System.out.println("Data Sorting Increasing Order : ");
+		aobj.Display();	
+		System.out.println("----------------------------------");
+		
+		aobj.BubbleSort('D'); // When you pass D as a parameter it will solve Drcreasing Order 
+		System.out.println("----------------------------------");
+		System.out.println("Data Sorting Decreasing Order : ");
+		aobj.Display();	
+		System.out.println("----------------------------------");
+	}	
+}
